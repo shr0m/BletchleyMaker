7f730404492ab14c678d018b4c7a5f68acef786f
Jan K <bx-one-family@outlook.com>
2025-03-30 19:33:10 +0100

    Add ViewCodes form

---
+++ BletchleyMaker/ViewCodes.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace BletchleyMaker
+{
+    partial class ViewCodes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewCodes));
+            clearAll = new Button();
+            removeSel = new Button();
+            SuspendLayout();
+            // 
+            // clearAll
+            // 
+            clearAll.Font = new Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            clearAll.Location = new Point(342, 315);
+            clearAll.Name = "clearAll";
+            clearAll.Size = new Size(75, 23);
+            clearAll.TabIndex = 0;
+            clearAll.Text = "Clear All";
+            clearAll.UseVisualStyleBackColor = true;
+            clearAll.Click += clearAll_Click;
+            // 
+            // removeSel
+            // 
+            removeSel.Font = new Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            removeSel.Location = new Point(200, 315);
+            removeSel.Name = "removeSel";
+            removeSel.Size = new Size(124, 23);
+            removeSel.TabIndex = 1;
+            removeSel.Text = "Remove Selected";
+            removeSel.UseVisualStyleBackColor = true;
+            removeSel.Click += removeSel_Click;
+            // 
+            // ViewCodes
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(428, 346);
+            Controls.Add(removeSel);
+            Controls.Add(clearAll);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "ViewCodes";
+            Text = "ViewCodes";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button clearAll;
+        private Button removeSel;
+    }
+}
---
+++ BletchleyMaker/ViewCodes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BletchleyMaker
+{
+    public partial class ViewCodes : Form
+    {
+        int currentPos = 0;
+        List<CheckBox> addCodes = new List<CheckBox>();
+        private Form1 mainForm;
+        public ViewCodes(List<string> savedCodes, Form1 form)
+        {
+            InitializeComponent();
+
+            mainForm = form;
+
+            for (int i = 0; i < savedCodes.Count; i++)
+            {
+                UpdateForm(savedCodes[i]);
+            }
+        }
+
+        private void removeSel_Click(object sender, EventArgs e)
+        {
+            for (int i = addCodes.Count - 1; i >= 0; i--)
+            {
+                if (addCodes[i].Checked)
+                {
+                    Controls.Remove(addCodes[i]);
+                    addCodes[i].Dispose();
+                    addCodes.RemoveAt(i);
+
+                    mainForm.RemoveCode(i);
+                }
+            }
+
+            this.Close();
+        }
+
+        public void UpdateForm(string add)
+        {
+            CheckBox cb = new CheckBox();
+            cb.Text = add;
+            cb.Location = new Point(10, 30 * currentPos + 10);
+            cb.AutoSize = true;
+            addCodes.Add(cb);
+            Controls.Add(cb);
+
+            currentPos++;
+        }
+
+        private void clearAll_Click(object sender, EventArgs e)
+        {
+            for (int i = addCodes.Count - 1; i >= 0; i--)
+            {
+                Controls.Remove(addCodes[i]);
+                addCodes[i].Dispose();
+                addCodes.RemoveAt(i);
+                mainForm.RemoveCode(i);
+            }
+
+            this.Close();
+        }
+    }
+}
