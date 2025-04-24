9f40a8cca036301983d72487138e39181065a38e
Jan <Jan@JK-LAPTOP>
2025-04-24 11:35:03 +0100

    Added character set editing

---
+++ BletchleyMaker/Forms/CharacterSet.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace BletchleyMaker.Forms
+{
+    partial class CharacterSet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            characterBox = new TextBox();
+            okButton = new Button();
+            cancelButton = new Button();
+            SuspendLayout();
+            // 
+            // characterBox
+            // 
+            characterBox.Location = new Point(75, 38);
+            characterBox.Name = "characterBox";
+            characterBox.Size = new Size(755, 39);
+            characterBox.TabIndex = 0;
+            // 
+            // okButton
+            // 
+            okButton.Location = new Point(536, 93);
+            okButton.Name = "okButton";
+            okButton.Size = new Size(150, 46);
+            okButton.TabIndex = 1;
+            okButton.Text = "OK";
+            okButton.UseVisualStyleBackColor = true;
+            okButton.Click += okButton_Click;
+            // 
+            // cancelButton
+            // 
+            cancelButton.Location = new Point(716, 93);
+            cancelButton.Name = "cancelButton";
+            cancelButton.Size = new Size(150, 46);
+            cancelButton.TabIndex = 2;
+            cancelButton.Text = "Cancel";
+            cancelButton.UseVisualStyleBackColor = true;
+            cancelButton.Click += cancelButton_Click;
+            // 
+            // CharacterSet
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(894, 151);
+            Controls.Add(cancelButton);
+            Controls.Add(okButton);
+            Controls.Add(characterBox);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "CharacterSet";
+            ShowIcon = false;
+            Text = "Edit Character Set";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox characterBox;
+        private Button okButton;
+        private Button cancelButton;
+    }
+}
---
+++ BletchleyMaker/Forms/CharacterSet.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BletchleyMaker.Forms
+{
+    public partial class CharacterSet : Form
+    {
+        private List<char> Chars;
+        private BletchleyMaker.Main form = null!;
+        public CharacterSet(List<char> chars, Main inpForm)
+        {
+            InitializeComponent();
+            this.AcceptButton = okButton;
+            this.CancelButton = cancelButton;
+            this.StartPosition = FormStartPosition.CenterParent;
+
+            Chars = chars;
+            form = inpForm;
+
+            characterBox.Text = string.Join("", Chars);
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            string text = characterBox.Text.ToUpper().Trim();
+            if (Validation(text))
+            {
+                form.SetChars(text.ToList<char>()); // Update Main's list
+                form.grid.SetChars(text.ToList<char>()); // Update Grid's list    
+                form.grid.Generate();           
+
+                this.Close();
+            }
+        }
+
+        private bool Validation(string input)
+        {
+            if (input.Length != 36)
+            {
+                MessageBox.Show("Character set must be 36 characters long", "Error", MessageBoxButtons.OK, MessageBoxIcon.None);
+                return false;
+            }
+
+            HashSet<char> seen = new HashSet<char>();
+            foreach (char c in input)
+            {
+                if (!seen.Add(c))
+                {
+                    MessageBox.Show("Duplicate characters exist", "Error", MessageBoxButtons.OK, MessageBoxIcon.None);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
