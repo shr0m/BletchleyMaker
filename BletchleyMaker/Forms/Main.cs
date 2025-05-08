d3ad493a2f46ded4b9f296a3fb63977e84f6c456
Jan <Jan@JK-LAPTOP>
2025-05-08 12:59:08 +0100

    Remove unnecessary line

--- BletchleyMaker/Forms/Main.cs
+++ BletchleyMaker/Forms/Main.cs
@@ -12,7 +12,6 @@ namespace BletchleyMaker
         private List<string> savedCodes;
         private ViewCodes view = null!;
         private List<char> Chars = new List<char> { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        private List<string> Rules = new List<string>();
 
         public Main(string[]? args = null)
         {
